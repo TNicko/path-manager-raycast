d7affbb954a05c8fba96259e0bc2363f47092eec
Nickolai Tchesnokov <nickolaitchesnokov@Nickolais-MacBook-Air-543.local>
2023-09-11 19:43:53 +0200

    minor updates

---
+++ src/add-path.tsx
@@ -0,0 +1,8 @@
+import { LocalStorage } from "@raycast/api";
+
+export default async function AddPath() {
+  //await LocalStorage.setItem("path", "~/desktop");
+  //const item = await LocalStorage.getItem<string>("path");
+  //console.log(item);
+} 
+
