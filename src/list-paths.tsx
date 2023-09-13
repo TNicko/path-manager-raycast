dc5e3c70470a27f2d0f659a6890d4dcbe64ac406
Nickolai Tchesnokov <nickolaitchesnokov@Nickolais-MacBook-Air-543.local>
2023-09-13 12:46:33 +0200

    Stored paths can now be searched using list paths command

--- src/list-paths.tsx
+++ src/list-paths.tsx
@@ -1,17 +1,56 @@
-import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import { ActionPanel, List, Action, environment } from "@raycast/api";
+import { useEffect, useState } from "react";
+import { promises as fs } from "fs";
+import path from "path";
 
+const STORAGE_PATH: string = path.join(environment.supportPath, "paths.json");
+ 
 export default function ListPaths() {
-return (
-    <List>
-      <List.Item
-        icon="list-icon.png"
-        title="Greeting"
-        actions={
-          <ActionPanel>
-            <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
-          </ActionPanel>
-        }
-      />
-    </List>
+  const [searchText, setSearchText] = useState("");
+  const [paths, setPaths] = useState<Record<string, string>>({});
+  const [filteredList, setFilteredList] = useState<{ alias: string; path: string }[]>([]);
+
+  useEffect(() => {
+    async function fetchPaths() {
+      try {
+        const rawData = await fs.readFile(STORAGE_PATH, "utf-8");
+        const parsedData: Record<string, string> = JSON.parse(rawData);
+        setPaths(parsedData);
+        setFilteredList(Object.entries(parsedData).map(([alias, path]) => ({alias, path})));
+      } catch (error: unknown) {
+        console.error("Failed to load paths:", error)
+        setPaths({});
+        setFilteredList([]);
+      }
+    }
+    fetchPaths();
+  }, []);
+
+  useEffect(() => {
+    const filtered = Object.entries(paths).filter(([alias, path]) => 
+    alias.includes(searchText) || path.includes(searchText)).map(([alias, path]) => ({alias, path}));
+    setFilteredList(filtered);
+  }, [searchText, paths]);
+
+  return (
+    <List
+      filtering={false}
+      onSearchTextChange={setSearchText}
+      navigationTitle="Search Paths"
+      searchBarPlaceholder="Search your paths"
+    >
+      {filteredList.map(({alias, path}) => (
+        <List.Item
+          key={alias}
+          title={alias}
+          subtitle={path}
+          actions={
+            <ActionPanel>
+              <Action title="Select" onAction={() => console.log(`${alias} selected`)} />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>  
   );
 }
